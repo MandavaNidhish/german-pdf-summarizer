40bcccc57d5909df0f909d486945bb96c5d8ab64
MandavaNidhish <141313979+MandavaNidhish@users.noreply.github.com>
2025-10-14 23:17:36 +0530

    Update utils.py

--- backend/utils.py
+++ backend/utils.py
@@ -16,10 +16,7 @@ def setup_logging():
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        handlers=[
-            
-            logging.StreamHandler()
-        ]
+        handlers=[logging.StreamHandler()]
     )
 
     logger = logging.getLogger(__name__)
